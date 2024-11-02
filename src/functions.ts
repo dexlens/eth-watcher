cf19183493a8c00663b4e79cd42febde8690a5fc
liquid <13621351+liqtags@users.noreply.github.com>
2024-11-02 23:36:07 +0000

    Setup Eth Watcher Websocket

--- src/functions.ts
+++ src/functions.ts
@@ -2,3 +2,10 @@
  * Shared functions file to import stuff from inside the shared protected folder
  * For access, get access to the Arweave for cloning the shared folder
  */
+
+/**
+ * Setup Eth Watcher Websocket
+ */
+export {
+    setupEthWatcherWebsocket
+} from "./shared/functions/setupEthWatcherWebsocket.ts";
